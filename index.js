d1dedbf74ff0d56d044e893cd6d623cf3daf7f56
CJ Cenizal <cj@cenizal.com>
2016-03-28 20:57:06 -0700

    Add tests.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 
-module.exports = function isSumPossible() {
-  return true;
+module.exports = function isSumPossible(summableNumbers, sumTarget, addendsTarget) {
+  function containsAddendsForSum(numbers, index, differenceFromTargetSum, addendsCount) {
+    // Inner terminating condition. Once we have involved the maximum
+    // number of addeds, return whether whether their difference from the
+    // sumTarget is 0 (i.e. they add up to the sumTarget).
+    if (addendsCount === 0) {
+      return differenceFromTargetSum === 0;
+    }
+
+    // Each recursion, test each subsequent number to see if we've found a match.
+    for (var i = index; i < numbers.length; i++) {
+      var newDifference = differenceFromTargetSum - numbers[i];
+
+      // Terminating condition. If true, we've found a matching sum.
+      if (containsAddendsForSum(numbers, i + 1, newDifference, addendsCount - 1)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // This is key. Sorting the numbers in increasing order allows to create
+  // combinations in this increasing order. This creates a distinct cut-off
+  // point, beyond which its impossible for a combination of addends to add up
+  // to the sumTarget. This allows us to use a for loop at the core of the
+  // recursive function because we can proceed sequentially through the
+  // summableNumbers.
+  var sortedSummableNumbers = summableNumbers.slice(0).sort();
+  return containsAddendsForSum(sortedSummableNumbers, 0, sumTarget, addendsTarget);
 }
--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,73 @@
 const isSumPossible = require('./index');
 
 describe('isSumPossible', () => {
-  it('returns true', () => {
-    expect(isSumPossible()).toBe(true);
+  describe('diagnostic test case', () => {
+    it('returns true', () => {
+      const summableNumbers = [18, 55, 28, 31, 38, 11, 40, 21, 60, 62];
+      const sumTarget = 198;
+      const addendsTarget = 4;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(true);
+    });
+  });
+
+  describe('returns true', () => {
+    it('when there is only one summable number and it is the target sum', () => {
+      const summableNumbers = [1];
+      const sumTarget = 1;
+      const addendsTarget = 1;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(true);
+    });
+
+    it('when the target sum is one number among many', () => {
+      const summableNumbers = [1, 2, 3];
+      const sumTarget = 3;
+      const addendsTarget = 1;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(true);
+    });
+
+    it('when we need many addends', () => {
+      const summableNumbers = [1, 2, 3, 4, 5];
+      const sumTarget = 9;
+      const addendsTarget = 3;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(true);
+    });
+
+    it('when we need many addends and the numbers are out of order', () => {
+      const summableNumbers = [5, 1, 4, 2, 3];
+      const sumTarget = 9;
+      const addendsTarget = 3;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(true);
+    });
+  });
+
+  describe('returns false', () => {
+    it('when there is only one summable number and it\'s not our target sum', () => {
+      const summableNumbers = [2];
+      const sumTarget = 1;
+      const addendsTarget = 1;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(false);
+    });
+
+    it('when there is only one summable number and we need many addends', () => {
+      const summableNumbers = [1];
+      const sumTarget = 1;
+      const addendsTarget = 2;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(false);
+    });
+
+    it('when there are many numbers but they don\'t add up to the target sum', () => {
+      const summableNumbers = [5, 2, 4];
+      const sumTarget = 3;
+      const addendsTarget = 1;
+      expect(isSumPossible(summableNumbers, sumTarget, addendsTarget))
+        .toBe(false);
+    });
   });
 });
